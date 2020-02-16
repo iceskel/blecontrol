9f5b1c513970d39b40b0de47b5055298b4413dce
Franc Hakani <fhakani@gmail.com>
2020-02-16 13:49:07 +0800

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import './App.css';
 
 function Connect(props: any) {
   const [charvalue, setcharvalue] = useState<any>(null);
+  const [ngage, setngage] = useState<any>(null);
 
   useEffect(() => {
     async function getChar() {
       if (!navigator.bluetooth) {
+        console.log('bnluetooth not found');
         return;
       }
       const service = await navigator.bluetooth.requestDevice({ filters: [{ services: ['19B10000-E8F2-537E-4F6C-D104768A1214']}]})
@@ -16,15 +18,17 @@ function Connect(props: any) {
   
       const char = await service.getCharacteristic("2A19");
       const value = await char.readValue();
+      console.log('bluietooth found');
       setcharvalue(value);
     }
     getChar();
-  }, []);
+  }, [ngage]);
 
 
   return (
     <div>
       value: {charvalue}
+      <button onClick={() => setngage(Math.random() * 5)}>lmao</button>
     </div>
   )
 }
